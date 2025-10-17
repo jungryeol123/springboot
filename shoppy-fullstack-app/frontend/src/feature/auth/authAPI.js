14851c8b96665517fb5ab640077f48c0f9959000
yoon <46823971@naver.com>
2025-10-17 12:30:21 +0900

    modified commit

--- shoppy-fullstack-app/frontend/src/feature/auth/authAPI.js
+++ shoppy-fullstack-app/frontend/src/feature/auth/authAPI.js
@@ -1,5 +1,5 @@
 import { login,logout } from "./authSlice.js";
-import { validateFormCheck } from "../../utils/validate.js";
+import { validateFormCheck,validateSignupFormCheck } from "../../utils/validate.js";
 import { axiosPost } from "../../utils/dataFetch.js";
 
 export const getLogin = (formData, param) => async(dispatch) => {
@@ -21,6 +21,32 @@ export const getLogin = (formData, param) => async(dispatch) => {
         }
         return false;
 }
+/**
+    id중복 체크
+*/
+export const getIdCheck = (id) => async(dispatch) => {
+    const data = {"id":id};
+    const url = "http://localhost:8080/member/idcheck";
+    const result = await axiosPost(url,data);
+    return result;
+}
+
+
+/**
+    signup
+*/
+export const getSignup = (formData, param) => async(dispatch) => {
+    let result = null;
+    if(validateSignupFormCheck(param)) {
+        const url = "http://localhost:8080/member/signup"
+        result = await axiosPost(url,formData);
+    }
+    return result;
+}
+
+/**
+    login
+*/
 export const getLogout = () => async (dispatch) => {
     dispatch(logout());
     return true;
