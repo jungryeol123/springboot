14eca6e0ce64c2d84470a5a4a5b99b4254dc35d7
yoon <46823971@naver.com>
2025-10-28 17:33:46 +0900

    modified commit

--- shoppy.sql
+++ shoppy.sql
@@ -600,7 +600,19 @@ insert into orders(
 	order_code, member_id, shipping_fee, discount_amount, total_amount, receiver_name, receiver_phone, zipcode, address1, address2, memo, odate
     )
  values();
+ 
+ select
+	:orderCode, pid, name as pname, size, qty, totalPrice as pid_total_price,
+    :discount
+    from view_cartlist;
     
+    set SQL_SAFE_UPDATES = 0;
+    delete  from orders;
+    delete  from order_detail;
+select * from order_detail;
+select * from orders;
+select * from view_cartList
+where cid in (3,4,5);
     
 
 
